01f24a9c6b8cbf823d56fb6599adc974ca2622ff
lamaSDA <Lama-4321-@hotmail.com>
2025-10-21 14:50:35 +0300

    HW-task03

--- src/test/java/Tests/Mentoring_Practices/Pojo_MAP/Task03/Task03.java
+++ src/test/java/Tests/Mentoring_Practices/Pojo_MAP/Task03/Task03.java
@@ -18,7 +18,7 @@ public class Task03 extends FakeRestBaseUrl  {
        // Implement CREATE (POST) - Add new activity
         System.out.println("--------------CREATE--------------");
         String dueDate = ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
-        ActivityPojo expectedData = new ActivityPojo(1, "Activity 1", dueDate , false);
+        ActivityPojo expectedData = new ActivityPojo(2, "Activity 2", dueDate , true);
 
 
         // Send POST request
@@ -30,7 +30,7 @@ public class Task03 extends FakeRestBaseUrl  {
         ActivityPojo actualPostData = postResponse.as(ActivityPojo.class);
         assertEquals(actualPostData.getTitle(), expectedData.getTitle());
         assertEquals(actualPostData.getCompleted(), expectedData.getCompleted());
-        System.out.println("CREATE operation successful");
+        System.out.println("-> CREATE operation successful");
 
         Integer id = actualPostData.getId(); // Store ID for later use
         System.out.println("Created ID = " + id);
@@ -45,13 +45,13 @@ public class Task03 extends FakeRestBaseUrl  {
         ActivityPojo actualGetData = getResponse.as(ActivityPojo.class);
         assertEquals(actualGetData.getTitle(), expectedData.getTitle());
         assertEquals(actualGetData.getCompleted(), expectedData.getCompleted());
-        System.out.println("READ operation successful");
+        System.out.println("-> READ operation successful");
 
 
         // Implement UPDATE (PUT) - Modify existing activity
         System.out.println("--------------UPDATE--------------");
         String currentDate = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
-        ActivityPojo updatedActivity = new ActivityPojo(1, "Updated Activity Title", currentDate, true);
+        ActivityPojo updatedActivity = new ActivityPojo(2, "Updated Activity Title", currentDate, true);
 
         // Send PUT request
         Response putResponse = RestAssured.given(spec).body(updatedActivity).put("/Activities/"+ updatedActivity.getId());
@@ -63,7 +63,7 @@ public class Task03 extends FakeRestBaseUrl  {
         assertEquals(responseBody.getId(), updatedActivity.getId());
         assertEquals(responseBody.getTitle(), updatedActivity.getTitle());
         assertEquals(responseBody.getCompleted(), updatedActivity.getCompleted());
-        System.out.println("UPDATE operation successful");
+        System.out.println("-> UPDATE operation successful");
 
 
 
@@ -72,7 +72,7 @@ public class Task03 extends FakeRestBaseUrl  {
         Response deleteResponse = given(spec).delete("/Activities/" + id);
         deleteResponse.prettyPrint();
         assertEquals(deleteResponse.statusCode(), 200);
-        System.out.println("DELETE operation successful");
+        System.out.println("-> DELETE operation successful");
 
 
     }
